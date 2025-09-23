c3a2d77de478eb8337f643cd47fb28c1b592bb14
KEVIN NSHUTI <nkevin881@gmail.com>
2025-09-23 16:46:15 +0200

    implementing basic routing in next.js

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,21 @@
 import React from "react";
+import Home from "@/pages/home";
+import About from "@/pages/about";
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/home">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
