799535dff5846c492bd4bdcfe6d6cb5ca67f202c
KEVIN NSHUTI <nkevin881@gmail.com>
2025-09-24 19:37:25 +0200

    implementiing a postcard component

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -8,3 +8,9 @@ export interface ButtonProps {
   size?: "small" | "medium" | "large";
   shape?: "rounded-sm" | "rounded-md" | "rounded-full";
 }
+
+export interface PostProps {
+  title: string;
+  content: string;
+  userID: string;
+}
--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,12 +1,44 @@
 import React from "react";
 import Header from "@/components/layout/Header";
-
+import PostCard from "@/components/common/PostCard";
+import { useState, useEffect } from "react";
+import { type PostProps } from "@/interfaces";
 const Posts = () => {
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts?_limit=10"
+      );
+      const data = await response.json();
+      const formattedPosts: PostProps[] = data.map((post: any) => ({
+        title: post.title,
+        content: post.body,
+        userID: post.userId,
+      }));
+      setPosts(formattedPosts);
+    };
+    fetchPosts();
+  }, []);
   return (
     <>
       <Header />
       <h1>Welcome to posts page</h1>
       <p>This is the posts page.</p>
+      <div>
+        {posts.length === 0 ? (
+          <p>posts loading ....</p>
+        ) : (
+          posts.map((post, idx) => (
+            <PostCard
+              key={idx}
+              title={post.title}
+              content={post.content}
+              userID={post.userID}
+            />
+          ))
+        )}
+      </div>
     </>
   );
 };
